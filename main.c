a9fe4f8331c5393f806b614b347b37622bfb244f
mustafa-ozan <86887685+mustafa-ozan@users.noreply.github.com>
2021-07-03 20:21:40 +0300

    Update main.c

--- main.c
+++ main.c
@@ -12,5 +12,25 @@ int main()
 
    int * ram = calloc (100000 * sizeof(int));
 
+
+   int temp = 0;
+   int loop_close = 1;
+
+   while( loop_close ){
+
+      printf("please enter input\n");
+      scanf("%d",&temp);
+
+      printf("If no more input please press 0, else press 1\n");
+      scanf("%d",&loop_close);
+
+      
+
+   }
+    
+
+
+
+
    return 0;
 }
