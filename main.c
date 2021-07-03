85b393142e6f55b483190818cbef82be2f7ed761
mustafa-ozan <86887685+mustafa-ozan@users.noreply.github.com>
2021-07-03 20:06:34 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,13 @@
 
 int main()
 {
-   
+   int * l1  = calloc (4 * sizeof(int));
 
+   int * l2  = calloc (16 * sizeof(int));
 
+   int * l3  = calloc (96 * sizeof(int));
+
+   int * ram = calloc (100000 * sizeof(int));
 
    return 0;
 }
